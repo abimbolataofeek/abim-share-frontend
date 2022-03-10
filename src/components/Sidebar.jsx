728b35daffe3caa1c5e6b9485a85c7888cc8630b
abimbolataofeek <65461446+abimbolataofeek@users.noreply.github.com>
2022-03-10 16:02:02 +0100

    sidebar ongoing

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,10 +4,19 @@ import { RiHomeFill } from 'react-icons/ri';
 import { IoIoArrowForward } from 'react-icons/io';
 
 import logo from '../assets/logo.png';
+const categories = [
+  { name: 'Animals' },
+  { name: 'Wallpapers' },
+  { name: 'Photography' },
+  { name: 'Coding' },
+  { name: 'Other' }
+]
+const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
+const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
 
 const Sidebar = ({ user, closeToggle }) => {
   const handeleCloseSidebar = () => {
-    if(closeToggle) closeToggle(false);
+    if (closeToggle) closeToggle(false);
   }
   return (
     <div className='flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar'>
@@ -15,6 +24,21 @@ const Sidebar = ({ user, closeToggle }) => {
         <Link to="/" className='flex px-5 gap-2 my-6 pt-1 w-190 items-center' onClick={handeleCloseSidebar}>
           <img src={logo} alt="logo-img" className='w-full' />
         </Link>
+        <div className='flex flex-col gap-5'>
+          <NavLink to="/" className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle} onClick={handeleCloseSidebar}>
+            <RiHomeFill />
+            Home
+          </NavLink>
+          <h3 className='mt-2 px-5 text-base 2x1:text-xl'>Discover categories</h3>
+          {categories.slice(0, categories.length - 1).map((category) => (
+            <NavLink to={`/category/${category.name}`}
+              className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle} onClick={handeleCloseSidebar}
+              key={category.name}
+            >
+              {category.name}
+            </NavLink>
+          ))}
+        </div>
 
       </div>
     </div>
