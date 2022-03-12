c791a69bc0bc2883775b78e23ea69ec64eb9d047
abimbolataofeek <65461446+abimbolataofeek@users.noreply.github.com>
2022-03-12 09:00:37 +0100

    sidebar done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,32 +15,41 @@ const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-
 const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
 
 const Sidebar = ({ user, closeToggle }) => {
-  const handeleCloseSidebar = () => {
+  const handleCloseSidebar = () => {
     if (closeToggle) closeToggle(false);
   }
   return (
     <div className='flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar'>
       <div className='flex flex-col'>
-        <Link to="/" className='flex px-5 gap-2 my-6 pt-1 w-190 items-center' onClick={handeleCloseSidebar}>
+        <Link to="/" className='flex px-5 gap-2 my-6 pt-1 w-190 items-center' onClick={handleCloseSidebar}>
           <img src={logo} alt="logo-img" className='w-full' />
         </Link>
         <div className='flex flex-col gap-5'>
-          <NavLink to="/" className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle} onClick={handeleCloseSidebar}>
+          <NavLink to="/" className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle} onClick={handleCloseSidebar}>
             <RiHomeFill />
             Home
           </NavLink>
           <h3 className='mt-2 px-5 text-base 2x1:text-xl'>Discover categories</h3>
           {categories.slice(0, categories.length - 1).map((category) => (
             <NavLink to={`/category/${category.name}`}
-              className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle} onClick={handeleCloseSidebar}
+              className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle} onClick={handleCloseSidebar}
               key={category.name}
             >
               {category.name}
             </NavLink>
           ))}
         </div>
-
       </div>
+      {user && (
+        <Link 
+        to={`user-profile/${user._id}`}
+          className="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3"
+          onClick={handleCloseSidebar}
+        >
+          <img src={user.image} className="w-10 h-10 rounded-full" alt='user-profile-image' />
+          <p>{user.userName}</p>
+        </Link>
+      )}
     </div>
   )
 }
